bd4ba0e452ca708ce7440b1a17bd40d3a281192a
dmitry <dkoshenkofff@mail.ru>
2023-10-22 00:51:04 +0300

    change

--- data-structures/deque/deque.go
+++ data-structures/deque/deque.go
@@ -3,3 +3,30 @@ package deque
 type Deque struct {
 	data []int
 }
+
+func NewQueue() *Deque {
+	return &Deque{
+		make([]int, 0, 10),
+	}
+}
+
+// PushFront - добавить в начало
+func (d *Deque) PushFront(val int) {
+	d.data = append(d.data[1:], d.data...)
+	d.data[0] = val
+}
+
+// PushBack - добавить в конец
+func (d *Deque) PushBack(val int) {
+	d.data = append(d.data, val)
+}
+
+// PopBack - удалить с конца
+func (d *Deque) PopBack() {
+	d.data = d.data[:len(d.data)-1]
+}
+
+// PopFront - удалить с начала
+func (d *Deque) PopFront() {
+	d.data = d.data[1:]
+}
--- task-2/main.go
+++ task-2/main.go
@@ -1,32 +1,5 @@
 package main
 
-import (
-	biHeap "bashnya_hw1/data-structures/binaryHeap"
-	"fmt"
-)
-
 func main() {
-	arr := []int{4, 5, 6, 3, 6}
-	h := biHeap.Build(arr)
 
-	h.Insert(7)
-	h.Insert(9)
-	h.Insert(8)
-
-	h.Insert(1)
-	h.Insert(7)
-	h.Insert(10)
-	h.Insert(11)
-
-	h.Insert(13)
-	h.Insert(12)
-	h.Insert(100)
-
-	fmt.Println(h)
-
-	h.ExtractTop()
-	fmt.Println(h)
-
-	h.ExtractTop()
-	fmt.Println(h)
 }
