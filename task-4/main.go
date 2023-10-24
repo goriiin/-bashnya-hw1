e36d689ad3e4f1a8ad5824ac2c8e3c8551a2cd1b
dmitry <dkoshenkofff@mail.ru>
2023-10-24 23:35:58 +0300

    task4

--- task-4/main.go
+++ task-4/main.go
@@ -1,5 +1,54 @@
 package main
 
+import "fmt"
+
+func createMatrix(size int) [][]int {
+	arr := make([]int, size*size)
+
+	matrix := make([][]int, size)
+	for i := range matrix {
+		matrix[i], arr = arr[:size], arr[size:]
+	}
+	return matrix
+}
+
+func fillMatrix(arr *[][]int) {
+	for i := range *arr {
+		var num int
+		for j := range *arr {
+			_, err := fmt.Scan(&num)
+			if err != nil {
+				return
+			}
+			(*arr)[i][j] = num
+		}
+	}
+}
+
 func main() {
+	var size int
+	_, err := fmt.Scan(&size)
+	if err != nil {
+		return
+	}
+
+	matrix := createMatrix(size)
+	fillMatrix(&matrix)
+
+	flag := true
+
+	for i := range matrix {
+		for j := range matrix {
+			if matrix[i][j] != matrix[j][i] {
+				flag = false
+			}
+		}
+	}
+
+	if flag {
+		fmt.Println(`YES`)
+	} else {
+		fmt.Println("NO")
+	}
 
 }
